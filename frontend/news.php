fe292c132c3e864877e6e8033ad0fe52fe4c3b25
mary0925555060 <32889078+mary0925555060@users.noreply.github.com>
2017-10-18 17:50:40 +0800

    1.0.1
    初版

--- frontend/news.php
+++ frontend/news.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>singlepost - Cake House</title>
+	<title>news - Cake House</title>
 	<?php require_once("template/files.php"); ?>
 </head>
 <body>
@@ -16,14 +16,14 @@
 					<h1>Single Post</h1>
 				</div>
 			</div>
-			<div class="singlepost">
+			<div class="news">
 				<div class="featured">
 					<img src="../images/strwberry-delights.jpg" alt="">
 					<h1>NEW CHILLS FOR SUMMER</h1>
 					<span>By Admin on November 28, 2023</span>
 					<p>You can replace all this text with your own text. You can remove any link to our website from this website template, you're free to use this website template without linking back to us. If you're having problems editing this website template, then don't hesitate to ask for help on the forum.</p>
 					<p>You can replace all this text with your own text. You can remove any link to our website from this website template, you're free to use this website template without linking back to us. If you're having problems editing this website template, then don't hesitate to ask for help on the forum.</p>
-					<a href="blog.php" class="load">back to blog</a>
+					<a href="news_list.php" class="load">back to blog</a>
 				</div>
 				<div class="sidebar">
 					<h1>Recent Posts</h1>
@@ -31,7 +31,7 @@
 					<h2>ON THE DIET</h2>
 					<span>By Admin on November 28, 2023</span>
 					<p>You can replace all this text with your own text. You can remove any link to our website from this website template.</p>
-					<a href="singlepost.php" class="more">Read More</a>
+					<a href="news.php" class="more">Read More</a>
 				</div>
 			</div>
 		</div>
--- frontend/news_list.php
+++ frontend/news_list.php
@@ -1,3 +1,9 @@
+<?php
+require_once("../connection/database.php");
+$sth = $db->query("SELECT * FROM news ORDER BY publishedDate DESC");
+$all_news = $sth->fetchAll(PDO::FETCH_ASSOC);
+$totalRows = count($all_news);
+ ?>
 <!doctype html>
 <!-- Website template by freewebsitetemplates.com -->
 <html>
@@ -13,32 +19,35 @@
 		<div id="body">
 			<div class="header">
 				<div>
-					<h1>Blog</h1>
+					<h1>最新消息</h1>
 				</div>
 			</div>
 			<div class="blog">
 				<div class="featured">
 					<ul>
+						<?php foreach($all_news as $row){ ?>
 						<li>
 							<img src="../images/new-chills.png" alt="">
 							<div>
-								<h1>NEW CHILLS FOR SUMMER</h1>
-								<span>By Admin on November 28, 2023</span>
-								<p>You can replace all this text with your own text. You can remove any link to our website from this website template, you're free to use this website template without linking back to us. If you're having problems editing this website template.</p>
-								<a href="singlepost.php" class="more">Read More</a>
+								<h1><?php echo $row['title']; ?></h1>
+								<span><?php echo $row['publishedDate']; ?></span>
+								<p><?php echo mb_substr($row['content'],0,50,"utf-8")."..."; ?></p>
+								<a href="news.php?newsID=<?php echo $row['newsID'];?>" class="more">Read More</a>
 							</div>
 						</li>
+					<?php } ?>
+
 						<li>
 							<img src="../images/berries.png" alt="">
 							<div>
 								<h1>BERRIES ON THE GROVE</h1>
 								<span>By Admin on November 28, 2023</span>
 								<p>You can replace all this text with your own text. You can remove any link to our website from this website template, you're free to use this website template without linking back to us. If you're having problems editing this website template.</p>
-								<a href="singlepost.php" class="more">Read More</a>
+								<a href="news.php" class="more">Read More</a>
 							</div>
 						</li>
 					</ul>
-					<a href="blog.php" class="load">Load More</a>
+					<a href="news_list.php" class="load">Load More</a>
 				</div>
 				<div class="sidebar">
 					<h1>Recent Posts</h1>
@@ -46,7 +55,7 @@
 					<h2>ON THE DIET</h2>
 					<span>By Admin on November 28, 2023</span>
 					<p>You can replace all this text with your own text. You can remove any link to our website from this website template.</p>
-					<a href="singlepost.php" class="more">Read More</a>
+					<a href="news.php" class="more">Read More</a>
 				</div>
 			</div>
 		</div>
